4bac96dd949a4f5a82ebf3bcadff40fd3f2fe2d2
gperea1 <42501744+gperea1@users.noreply.github.com>
2020-08-26 14:16:47 -0700

    Most recent ML file

--- deltaSOC.m
+++ deltaSOC.m
@@ -2,10 +2,25 @@
 % German Perea
 % This space is for making predictions for 
 % change of SOC
-M = trainingnormalized;
-test_data = testnormalized1;
 
-% Comments on data
+
+%You will need to import the following
+%They are provided as CSV files 
+
+%% Uncomment for Data training and testing with 10 outliers in 
+
+%M = trainingnormalized1;
+%test_data = testnormalized1;
+
+
+%% Data is for training and testing without outliers 
+% This data gave lower metrics 
+M = trainnormalized2;
+test_data = testnormalized2;
+
+%% TRAINING OUR MODEL in series-parallel architecture 
+
+% The index is in date format: 'yyyy-MM-dd HH:mm:ss'
 % Columns 
 
 % 1st = timestamp
@@ -19,15 +34,11 @@ test_data = testnormalized1;
 % 9th = Weeks 
 
 % Extra Values from table
+
 N = M{:,:};
 
-
 % Transpose the values
-ts =N(:,1)';
-
-dat = datetime(ts, 'InputFormat','yyyy-MM-dd HH:mm:ss','Format', 'yyyy-MM-dd HH:mm:ss');
-
-% Initially it was  {str2double(N(:,2)')}
+% Converting the values into a correct format for training 
 
 bat = str2double(N(:,2)');
 SOC =  str2double(N(:,3)');
@@ -40,100 +51,106 @@ week = str2double(N(:,9)');
 hour = str2double(N(:,10)');
 day = str2double(N(:,11)');
 
+% Trying different combination of features 
 %input = [bat;amps;volts];
 
 %input = [bat;amps;volts;building;SOC;adjusted];
 %input = bat;
 %input  = [bat;building];
+input = [bat;amps;volts];
+ 
+% narxnet(inputDelays,feedbackDelays,hiddenSizes,feedbackMode,trainFcn)
 
-input = [bat];
+% inputDelays = 1:2
+% feedbackDelays = 1:2
+% hiddenSizes = 10 (neurons in the hidden layer)
+% feedbackMode = default "open"
+% trainFcn = default "Levenberg-Marquardt backpropagation"
 
-% Creating our NARX NN
-
-narx_net = narxnet(1:2,1:2,15);
+narx_net = narxnet(1:2,1:2,10);
 narx_net.divideFcn = '';
 
 % Set the number of minimmum gradient moving step 
 
-% We have different methods if we have specific preferences
-% Setting the gradient 
-narx_net.trainParam.min_grad = 1e-10;
+narx_net.trainParam.min_grad = 1e-15;
 
 
 [p,Pi,Ai,t] = preparets(narx_net,con2seq(input),{},deSOC);
 
 
 % Input the testing data here 
-
 % Plugging the testing here
+% The following is for the testing data 
 
-% The following is the cleaning for our TESTING data 
-test_da = test_data{:,:}; % correcting format
-test_bat = test_da(:,2)'; % Battery 
-test_SOC =  test_da(:,3)'; % State of charge
-test_building = test_da(:,4)'; % Building 
-test_adjusted = test_da(:,5)'; % Adjusted (Building + Battery)
-test_amps = test_da(:,6)'; % Ampere
-test_volts = test_da(:,7)'; % Voltage
-test_deSOC = con2seq(test_da(:,8)'); % Change in State of Charge 
-test_week = test_da(:,9)'; % Week of the year
-test_hour = test_da(:,10)'; % Hour of the day
-test_day = test_da(:,11)'; % Day of the week
+test_da = test_data{:,:};
+test_da = str2double(test_da);
+test_bat = test_da(:,2)';
+test_SOC =  test_da(:,3)';
+test_building = test_da(:,4)';
+test_adjusted = test_da(:,5)';
+test_amps = test_da(:,6)';
+test_volts = test_da(:,7)';
+test_deSOC = con2seq(test_da(:,8)');
 
-% Features we would like to select 
-in2 = [test_bat];
+% The following are times
+test_week = test_da(:,9)';
+test_hour = test_da(:,10)';
+test_day = test_da(:,11)';
 
 [narx_net, tr] = train(narxnet,p,t,Pi);
 yp = sim(narx_net,p,Pi);
 e = cell2mat(yp) - cell2mat(t);
 TS1 = size(t,2);
+
+% Metrics for training
+
 train_mse = mse(e);
 train_rmse = sqrt(train_mse);
 train_mae = mae(e);
-
 figure(2)
 
 plot(1:TS1,cell2mat(t),'b',1:TS1,cell2mat(yp),'r')
 legend('True','Predicted')
-title('Inputs: Battery and Week')
+title('Training data: Battery(kW), Ampere(amps), Voltage(Volts)')
 xlabel('Row in data')
 ylabel('Normalized deltaSOC')
 
 
 
-%% This section covers how to test our model
+%% TESTING OUR MODEL in Parallel Architecture 
 
 % Closed_Loop
-
-
+in2 = [test_bat;test_amps;test_volts];
 figure(3)
-
-
-narx_net_closed = closeloop(narx_net);
+[narx_net_closed] = closeloop(narx_net);
 y1 = test_deSOC;
 u1 = con2seq(in2);
 [p1,Pi1,Ai1,t1] = preparets(narx_net_closed,u1,{},y1);
 yp1 = narx_net_closed(p1, Pi1,Ai1);
 TS = size(t1, 2);
 
-plot(1:TS,cell2mat(t1),'b',1:TS,cell2mat(yp1),'r')
+%yp1 = sim(narx_net_closed,Pi1,Ai1);
 e2 = cell2mat(yp1) - cell2mat(t1);
 
 plot(1:TS,cell2mat(t1),'b',1:TS,cell2mat(yp1),'r')
 legend('True','Predicted')
-title('deltaSOC: with Battery(kW) as input')
+title('Testing data: Battery(kW), Ampere(amps), Voltage(Volts)')
 xlabel('Row in data')
 ylabel('Normalized deltaSOC')
-% 
 
 % For testing purposes
-Y = narx_net_closed(p1, Pi1, Ai1);
 
-% Metrics
+% Metrics for testing 
+Y = narx_net_closed(p1, Pi1, Ai1);
 MSE = mse(narx_net_closed, t1, Y);
 RMSE = sqrt(MSE);
-% MAE 
 MAE = mae(e2);
+
+
+
+
+
+
 
 
 
